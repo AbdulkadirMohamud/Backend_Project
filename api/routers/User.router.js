87b4a839b43bc8ecbad70bd52a3e107ba2737d12
Abdulkadir <qansax30@hotmail.com>
2023-12-14 19:04:35 +0100

    we worked on that part of the project

---
+++ api/routers/User.router.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { login, signup, user, userDelete, userUpdate, users } from '../controllers/User.controller.js'
+import verifyToken from '../middleware/verifyToken.js'
+
+const router = express.Router()
+// router endpoints
+router.post("/signup",  signup)
+router.post("/login", login)
+router.get("/", users)
+router.get("/user", verifyToken, user)
+router.put("/update", verifyToken, userUpdate)
+router.delete("/delete", verifyToken, userDelete)
+export default router
+
+
+
+
+
+
+
--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRouter from './routers/User.router.js'
 
 const server = express()
 
 server.use(express.json())
-
-server.get("/", (req,res) => {
-    res.status(200).json({message:"it works"})
+dotenv.config()
+// connect mongoDB
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected MongoDB")
+}).catch((error) => {
+    console.log("Connecting MongoDB Error:", error)
 })
+
+// routes
+server.use("/api/users", userRouter)
 
 export default server
